0209b0924b7d82a1d398393fb7345eadac10e0d0
Triska Mahfud K <triska.triskacode@gmail.com>
2022-02-17 09:24:03 +0100

    setup idcloudhoost

--- libs/shared/environments/src/lib/shared-environments.tsx
+++ libs/shared/environments/src/lib/shared-environments.tsx
@@ -1,7 +1,7 @@
 export const config = {
     api: {
-      SERVER_HTTP: 'http://localhost:4200',
-      SERVER_GRAPHQL: 'http://localhost:4200/graphql',
+      SERVER_HTTP: 'http://103.181.142.87:4200',
+      SERVER_GRAPHQL: 'http://103.181.142.87:4200/graphql',
     },
     cookie: {
       TOKEN_COOKIE_NAME: 'token',
